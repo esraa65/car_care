178cf26d068a680e060391ef98898ea732d7c78c
esraa65 <esraakhaled6565@gmail.com>
2024-08-22 13:55:34 +0300

    add widget in ui

---
+++ lib/presentation/view/workshop_details/rating_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RatingView extends StatelessWidget {
+  const RatingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

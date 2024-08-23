51c1949da2cf3326c2acddd63d243b7f4a164a35
esraa65 <esraakhaled6565@gmail.com>
2024-08-23 21:30:07 +0300

    merge

--- lib/app/routing/app_routing.dart
+++ lib/app/routing/app_routing.dart
@@ -2,6 +2,7 @@ import 'package:car_care/app/routing/routes.dart';
 import 'package:car_care/presentation/view/Car_Care/car_care_screen.dart';
 import 'package:car_care/presentation/view/layout/layout.dart';
 import 'package:car_care/presentation/view/workshop/workshop_view.dart';
+import 'package:car_care/presentation/view/workshop_details/workshop_details_view.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
